a05990d14ca0345904304c6456fdd50201804b57
Keoni <joonhyuk5972@gmail.com>
2024-03-05 12:42:54 -1000

    even more love2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,27 +3,27 @@ import { Nav, NavItem, NavLink } from "reactstrap"
 
 const Navbar = ({ current_user }) => {
   return (
-<Nav className="nav">
-  {current_user && (
-    <NavItem>
-      <input type="button" value="Logout" />
-    </NavItem>
-  )}
-  {!current_user && (
-    <>
-      <NavItem>
-        <NavLink to="/login" className="nav-link">
-          Log In
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink to="/signup" className="nav-link">
-          Sign Up
-        </NavLink>
-      </NavItem>
-    </>
-  )}
-</Nav>
+    <Nav className="nav">
+      {current_user && (
+        <NavItem>
+          <input type="button" value="Logout" />
+        </NavItem>
+      )}
+      {!current_user && (
+        <>
+          <NavItem>
+            <NavLink to="/login" className="nav-link">
+              Log In
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/signup" className="nav-link">
+              Sign Up
+            </NavLink>
+          </NavItem>
+        </>
+      )}
+    </Nav>
   )
 }
 
